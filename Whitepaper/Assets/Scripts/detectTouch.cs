f0084189354121a6c810fcd31d3b8feb08f1aa15
Amber <aarawson@gmail.com>
2018-07-19 00:04:49 -0400

    Added popup controlled by click

--- Whitepaper/Assets/Scripts/detectTouch.cs
+++ Whitepaper/Assets/Scripts/detectTouch.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class detectTouch : MonoBehaviour {
-
+	public SpriteRenderer popup_sr;
 	// Use this for initialization
 	void Start () {
-
+		popup_sr = GameObject.Find("popup").GetComponent<SpriteRenderer>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0) {
-			Debug.Log(Input.touchCount);
-			Debug.Log("Touching " + Input.touches[0].position);
-			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.touches[0].position), Vector2.zero);
-			if(hit.collider != null && hit.collider.name != null){
-				Debug.Log("I hit something!");
+			if(popup_sr.sortingOrder == 0){
+				popup_sr.sortingOrder = -1;
+			}else{
+				Debug.Log(Input.touchCount);
+				Debug.Log("Touching " + Input.touches[0].position);
+				RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.touches[0].position), Vector2.zero);
+				if(hit.collider != null && hit.collider.name != null){
+					Debug.Log("I hit something!");
+					if(popup_sr.sortingOrder == -1){
+						popup_sr.sortingOrder = 0;
+					}
+				}
 			}
 		}
 	}
