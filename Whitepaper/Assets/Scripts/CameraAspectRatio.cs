2bd26e275787bebf39a83aa7ea38370e30b1d569
Amber <aarawson@gmail.com>
2018-07-18 21:13:25 -0400

    Fixed colliders

--- Whitepaper/Assets/Scripts/CameraAspectRatio.cs
+++ Whitepaper/Assets/Scripts/CameraAspectRatio.cs
@@ -10,27 +10,30 @@ public class CameraAspectRatio : MonoBehaviour {
 
         Camera camera = GetComponent<Camera>();
 
-        RectTransform test_page = pages[0].GetComponent<RectTransform>();
-        Rect test_page_dim = test_page.rect;
+		foreach (GameObject page in pages) {
+			RectTransform test_page = page.GetComponent<RectTransform>();
+	        Rect test_page_dim = test_page.rect;
 
-        pages[0].transform.position = new Vector2(0, 0);
-        test_page.position = new Vector2(0, 0);
+	        page.transform.position = new Vector2(0, 0);
+	        test_page.position = new Vector2(0, 0);
 
-        float page_ratio = (test_page_dim.width / test_page_dim.height);
+	        float page_ratio = (test_page_dim.width / test_page_dim.height);
 
-        float window_ratio = (float) Screen.width / (float) Screen.height;
+	        float window_ratio = (float) Screen.width / (float) Screen.height;
 
-        Camera.main.orthographicSize = (test_page_dim.height / 2.0f) * page_ratio;
-        Camera.main.aspect = window_ratio;
+	        Camera.main.orthographicSize = (test_page_dim.height / 2.0f) * page_ratio;
+	        Camera.main.aspect = window_ratio;
 
-        Debug.Log(test_page.localScale);
-        //x * test_page.height = Screen.height;
-        Debug.Log(page_ratio / window_ratio);
-        test_page.localScale = new Vector3(page_ratio, page_ratio, 1.0f);
+	        Debug.Log(test_page.localScale);
+	        //x * test_page.height = Screen.height;
+	        Debug.Log(page_ratio / window_ratio);
+	        test_page.localScale = new Vector3(page_ratio, page_ratio, 1.0f);
+		}
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
