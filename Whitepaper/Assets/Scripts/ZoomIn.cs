c30a340cdd53d15b2b4ec273af4eca95e3c6f20d
Amber <aarawson@gmail.com>
2018-07-23 00:10:14 -0400

    Added zoom scripts

--- Whitepaper/Assets/Scripts/ZoomIn.cs
+++ Whitepaper/Assets/Scripts/ZoomIn.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ZoomIn : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0) {
-			while (Camera.main.fieldOfView <=125 || Camera.main.orthographicSize <=20) {
-				Camera.main.fieldOfView += 2;
-				Camera.main.orthographicSize += 0.5;
+			while (Camera.main.fieldOfView>2 || Camera.main.orthographicSize>=1) {
+				Camera.main.fieldOfView -= 2;
+				Camera.main.orthographicSize -= 0.5f;
 			}
 		}
 	}
--- Whitepaper/Assets/Scripts/ZoomOut.cs
+++ Whitepaper/Assets/Scripts/ZoomOut.cs
@@ -6,15 +6,15 @@ public class ZoomOut : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0) {
-			while (Camera.main.fieldOfView>2 || Camera.main.orthographicSize>=1) {
-				Camera.main.fieldOfView -= 2;
-				Camera.main.orthographicSize -= 0.5;
+			while (Camera.main.fieldOfView <=125 || Camera.main.orthographicSize <=20) {
+				Camera.main.fieldOfView += 2;
+				Camera.main.orthographicSize += 0.5f;
 			}
 		}
 	}
