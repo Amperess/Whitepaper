fa02a166865e7a8a95e22ea9dd0dd73045225316
11 <drudolph914@gmail.com>
2018-08-03 00:02:26 -0400

    prefabs folder in resources

---
+++ Whitepaper/Assets/Scripts/StateManagement/States/Intro/Intro1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Application {
+    public class Intro1 : State {
+        
+        public Intro1() {
+        }
+
+        public override void Destroy() {
+            throw new NotImplementedException();
+        }
+
+        public override void Init() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Whitepaper/Assets/Scripts/StateManagement/States/Intro/Intro2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Application {
+    public class Intro2 : State{
+        
+        public Intro2() {
+        }
+
+        public override void Destroy() {
+            throw new NotImplementedException();
+        }
+
+        public override void Init() {
+            throw new NotImplementedException();
+        }
+    }
+}
